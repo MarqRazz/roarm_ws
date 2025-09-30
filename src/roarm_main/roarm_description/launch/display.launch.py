2bae322ef7dd6c341397cab94f059109ae663adc
marqrazz <marq.razz@gmail.com>
2025-09-30 15:19:19 -0600

    Update to Jazzy

--- src/roarm_main/roarm_description/launch/display.launch.py
+++ src/roarm_main/roarm_description/launch/display.launch.py
@@ -39,13 +39,6 @@ def generate_launch_description():
         ]
     )
 
-    joint_state_publisher_node = Node(
-        condition=UnlessCondition(show_gui),
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher'
-    )
-
     joint_state_publisher_gui_node = Node(
         condition=IfCondition(show_gui),
         package='joint_state_publisher_gui',
@@ -64,7 +57,6 @@ def generate_launch_description():
     return LaunchDescription([
         gui_arg,
         robot_state_publisher_node,
-        joint_state_publisher_node,
-        joint_state_publisher_gui_node,
+        # joint_state_publisher_gui_node, # commented out to visualize pose from hardware
         rviz_node
     ])
--- src/roarm_main/roarm_driver/roarm_driver/roarm_driver.py
+++ src/roarm_main/roarm_driver/roarm_driver/roarm_driver.py
@@ -15,31 +15,51 @@ class RoarmDriver(Node):
         baud_rate = self.get_parameter('baud_rate').value
         self.roarm_type = os.environ['ROARM_MODEL']
         self.roarm = roarm(roarm_type=self.roarm_type, port=serial_port_name, baudrate=baud_rate)
-        
-        self.joint_states_sub = self.create_subscription(JointState, 'joint_states', self.joint_states_callback, 10)    
+
+        self.joint_cmd_sub = self.create_subscription(JointState, 'roarm_joint_cmds', self.joint_cmd_callback, 10)
+        self.joint_state_pub = self.create_publisher(JointState, 'joint_states', 10)
         self.led_ctrl_sub = self.create_subscription(Float32, 'led_ctrl', self.led_ctrl_callback, 10)
+
+        timer_period = 0.05  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def timer_callback(self):
+        try:
+            feedback = self.roarm.joints_radian_get()
+        except SerialException as e:
+            self.get_logger().error(f"{e}")
+            return
+        states = JointState()
+        states.header.stamp = self.get_clock().now().to_msg()
+        states.name = ['base_link_to_link1', 'link1_to_link2', 'link2_to_link3', 'link3_to_link4', 'link4_to_link5', 'link5_to_gripper_link']
+        for i in range(0,6):
+            states.position.append(feedback[i])
+            states.velocity.append(0.0)
+            states.effort.append(0.0)
+        self.joint_state_pub.publish(states)
         
+
     def handle_m2_joint_states(self,name,position):
         base = position[name.index('base_link_to_link1')]
         shoulder = position[name.index('link1_to_link2')]
         elbow =  position[name.index('link2_to_link3')]
         hand =  position[name.index('link3_to_gripper_link')]
-                
-        radians = [base, shoulder, elbow, hand]  
+
+        radians = [base, shoulder, elbow, hand]
         return radians
-    
+
     def handle_m3_joint_states(self,name,position):
         base = position[name.index('base_link_to_link1')]
         shoulder = position[name.index('link1_to_link2')]
         elbow =  position[name.index('link2_to_link3')]
         wrist =  position[name.index('link3_to_link4')]
-        roll = position[name.index('link4_to_link5')] 
+        roll = position[name.index('link4_to_link5')]
         hand =  position[name.index('link5_to_gripper_link')]
-                
-        radians = [base, shoulder, elbow, wrist, roll, hand]  
+
+        radians = [base, shoulder, elbow, wrist, roll, hand]
         return radians
-        
-    def joint_states_callback(self, msg):
+
+    def joint_cmd_callback(self, msg):
 
         header = {
             'stamp': {
@@ -48,7 +68,7 @@ class RoarmDriver(Node):
             },
             'frame_id': msg.header.frame_id,
         }
-        
+
         name = msg.name
         position = msg.position
         velocity = msg.velocity
@@ -58,18 +78,17 @@ class RoarmDriver(Node):
             "roarm_m2": self.handle_m2_joint_states,
             "roarm_m3": self.handle_m3_joint_states,
         }
-        radians = switch_dict[self.roarm_type](name,position)   
-        
+        radians = switch_dict[self.roarm_type](name,position)
+
         try:
             self.roarm.joints_radian_ctrl(radians=radians,speed=1000,acc=50)
-            self.roarm.feedback_get()
         except SerialException as e:
             self.get_logger().error(f"{e}")
-            
+
     def led_ctrl_callback(self, msg):
         led = msg.data
         self.roarm.led_ctrl(led=led)
-        
+
 def main(args=None):
     rclpy.init(args=args)
     roarm_driver = RoarmDriver()
